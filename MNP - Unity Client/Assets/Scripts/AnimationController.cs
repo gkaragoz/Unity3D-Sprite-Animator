26dbf16f914fcc381dafe166684ea8291bd77036
gkaragoz <gkaragoz96@gmail.com>
2018-01-11 02:12:43 +0300

    Add Animation Controller script as stupid.

---
+++ MNP - Unity Client/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+	public Sprite []sprites;
+
+	public float frameRate = 0.2f;
+
+	public float nextFrameTime;
+
+	public int currentFrameIndex;
+
+	public SpriteRenderer spriteRenderer;
+
+	void Start() {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	void Update() {
+		if (Time.time > nextFrameTime) {
+			nextFrameTime = Time.time + frameRate;
+
+			if (currentFrameIndex < sprites.Length) {
+				spriteRenderer.sprite = sprites [currentFrameIndex++];
+			} else {
+				currentFrameIndex = 0;
+			}
+		}
+	}
+
+}
