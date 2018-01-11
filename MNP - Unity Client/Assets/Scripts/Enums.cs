565e65bd44d877ea04628936acdc72ba943dedb7
gkaragoz <gkaragoz96@gmail.com>
2018-01-11 13:11:49 +0300

    Create Enums.cs script

---
+++ MNP - Unity Client/Assets/Scripts/Enums.cs
@@ -0,0 +1,17 @@
+public static class Enums {
+    public enum AnimationState {
+        Idle,
+        Run
+    }
+
+    public enum Direction {
+        Up,
+        Down,
+        Right,
+        Left,
+        UpRight,
+        UpLeft,
+        DownRight,
+        DownLeft
+    }
+}
